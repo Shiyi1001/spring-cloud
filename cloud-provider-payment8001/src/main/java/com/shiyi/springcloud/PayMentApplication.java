29d27841d20a0f4daa12cea21b8fdd2286f3c142
fengling <fengling@shindata.com>
2021-11-02 16:59:11 +0800

    init

---
+++ cloud-provider-payment8001/src/main/java/com/shiyi/springcloud/PayMentApplication.java
@@ -0,0 +1,18 @@
+package com.shiyi.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName PayMentApplication
+ * @Description
+ * @Author FengL
+ * @Date 2021/11/02 16:38
+ * @Version V1.0
+ */
+@SpringBootApplication
+public class PayMentApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PayMentApplication.class, args);
+    }
+}
